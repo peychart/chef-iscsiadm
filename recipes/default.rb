a68817b17eb2fad6651cb1baefa89d85daebba1a
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-03-25 11:08:25 -1000

    reduction de la syntaxe de cmd iscsiadm a celle semble-t-il tolérée par debian

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-if node['chef-iscsiadm']['ip']
+if node['chef-iscsiadm'] != {}
 
   package 'open-iscsi' do
     package_name 'open-iscsi'
